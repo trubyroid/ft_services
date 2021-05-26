825ae673b46be83953bd305b4cf112c4352baa15
Toluvia Ruby <truby@pr-c1.kzn.21-school.ru>
2021-05-26 23:16:29 +0300

    completed

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-kubectl delete deploy nginx-deployment
-kubectl delete svc nginx-svc
-kubectl delete deploy wp-deployment
-kubectl delete svc wp-serv
-kubectl delete deploy php-deployment
-kubectl delete svc php-serv
-kubectl delete deploy mysql-deployment
-kubectl delete svc sql-serv
-kubectl delete deploy grafana-deployment
-kubectl delete svc grafana-serv
-kubectl delete deploy influxdb-deployment
-kubectl delete svc influxdb-serv
-kubectl delete deploy ftps-deployment
-kubectl delete svc ftps-svc
+# kubectl delete deploy nginx-deployment
+# kubectl delete svc nginx-svc
+# kubectl delete deploy wp-deployment
+# kubectl delete svc wp-serv
+# kubectl delete deploy php-deployment
+# kubectl delete svc php-serv
+# kubectl delete deploy mysql-deployment
+# kubectl delete svc sql-serv
+# kubectl delete deploy grafana-deployment
+# kubectl delete svc grafana-serv
+# kubectl delete deploy influxdb-deployment
+# kubectl delete svc influxdb-serv
+# kubectl delete deploy ftps-deployment
+# kubectl delete svc ftps-svc
 
 minikube start --vm-driver=virtualbox --disk-size="10000mb" --memory="4000mb"
 minikube addons enable metallb
